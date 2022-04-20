d689fa7641363ffc13922e9fbc8ffb6d031a9406
root <Ajayijoshuamayokun>
2022-04-20 14:06:44 -0700

    My commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
@@ -27,8 +26,9 @@ printf("Last digit of %i is %i and is 0\n", n, n % 10);
 
 else
 {
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+printf("Last digit of %i is %i and is less than 6 and not 0\n', n, n % 10);
 }
 
-return(0);
+return (0);
 }
+
