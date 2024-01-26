976b8962f67c40c3256c9e55c8366f91568dad63
Ajayijoshuamayokun <ajajyijoshuamayokun@gmail.com>
2024-01-26 07:33:37 -0800

    still coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,25 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific message to the standard error using the write
+ * system call.
+ *
+ * Return: Always 1 (Error code)
+ */
 int main(void)
 {
-    const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-    ssize_t written_bytes = write(2, message, 59); 
+	const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	ssize_t written_bytes;
 
-    if (written_bytes == -1) {
-        return 1;
-    }
+	written_bytes = write(2, message, 59);  /* 59 is the length of the message */
 
-    return 0;
+	if (written_bytes == -1)
+	{
+		return 1;
+	}
+
+	return 0;
 }
 
