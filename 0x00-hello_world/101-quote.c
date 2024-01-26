083a1425cb9178a542b8554e4ff497cdbd40f906
Ajayijoshuamayokun <ajajyijoshuamayokun@gmail.com>
2024-01-26 07:28:48 -0800

    still coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-    ssize_t written_bytes = write(2, message, 59);  // 59 is the length of the message
+    ssize_t written_bytes = write(2, message, 59); 
 
     if (written_bytes == -1) {
         return 1;
